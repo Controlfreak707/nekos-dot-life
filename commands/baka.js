626024509812d9da93cc98a46f68e1d62b26559c
ControlFreak707 <controlfreak707@gmail.com>
2020-11-23 13:04:12 -0800

    Add files via upload

---
+++ commands/baka.js
@@ -0,0 +1,11 @@
+module.exports = {
+  command: "baka",
+  description: "Gets a URL of a baka image/gif.",
+  executor: async (args, nekos) => {
+    const { url } = await nekos.sfw.baka();
+    return {
+      send: true,
+      result: url,
+    };
+  },
+};
