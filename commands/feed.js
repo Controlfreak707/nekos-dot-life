aafc661952d23964f74a765cadf5055eb697ab15
Aiden Baker <33640860+CaptainSnowball@users.noreply.github.com>
2021-02-12 13:05:28 -0500

    I broek it

--- commands/feed.js
+++ commands/feed.js
@@ -1,9 +1,11 @@
-export const command = "feed";
-export const description = "Gets the URL of a feeding image/gif.";
-export async function executor(args, nekos) {
-  const { url } = await nekos.sfw.feed();
-  return {
-    send: true,
-    result: url,
-  };
-}
+module.exports = {	export const command = "feed";
+  command: "feed",	export const description = "Gets the URL of a feeding image/gif.";
+  description: "Gets the URL of a feeding image/gif.",	export async function executor(args, nekos) {
+  executor: async (args, nekos) => {	  const { url } = await nekos.sfw.feed();
+    const { url } = await nekos.sfw.feed();	  return {
+    return {	    send: true,
+      send: true,	    result: url,
+      result: url,	  };
+    };	}
+  },	
+};
