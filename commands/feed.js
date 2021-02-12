506a842de75af2f1d6f7dd50a9d7329787a622c7
Aiden Baker <33640860+CaptainSnowball@users.noreply.github.com>
2021-02-12 13:08:26 -0500

    Emergency Fix

--- commands/feed.js
+++ commands/feed.js
@@ -1,11 +1,11 @@
-module.exports = {	export const command = "feed";
-  command: "feed",	export const description = "Gets the URL of a feeding image/gif.";
-  description: "Gets the URL of a feeding image/gif.",	export async function executor(args, nekos) {
-  executor: async (args, nekos) => {	  const { url } = await nekos.sfw.feed();
-    const { url } = await nekos.sfw.feed();	  return {
-    return {	    send: true,
-      send: true,	    result: url,
-      result: url,	  };
-    };	}
-  },	
+module.exports = {
+  command: "feed",
+  description: "Gets the URL of a feeding image/gif.",
+  executor: async (args, nekos) => {
+    const { url } = await nekos.sfw.feed();
+    return {
+      send: true,
+      result: url,
+    };
+  },
 };
